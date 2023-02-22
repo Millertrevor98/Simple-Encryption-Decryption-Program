fa3958a20327f10b738977ec0cf3661ebbaa67fe
Trevor <78606684+Millertrevor98@users.noreply.github.com>
2023-02-22 18:34:14 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //change the key to get different encryption codes
 	int key= 5;
-    String sent="Deez Nutz";
+    String sent="Hello World";
     char[] chars= sent.toCharArray();
     for(char x:chars){
         //increment or decrement the key to encrypt or decrypt the message
